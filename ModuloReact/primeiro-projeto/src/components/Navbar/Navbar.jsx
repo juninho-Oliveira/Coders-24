ca182eb5bc33ddeb25175374de24b1536f006080
junior <junior.oliveira05@icloud.com>
2024-06-07 11:58:32 -0300

    add components

---
+++ ModuloReact/primeiro-projeto/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import logoImg from '../../assets/img/logo.png'
+
+import "./style.css"
+
+class Navbar extends React.Component {
+    render () {
+        return (
+            <header>
+      <nav id="navbar">
+        <div className="nav-brand">
+          <img src={logoImg} alt="log"/>
+          <h1>Space Flight News</h1>
+        </div>
+
+        <ul className="nav-list">
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Trending</a></li>
+          <li><a href="/">Categories</a></li>
+          <li><a href="/">About us</a></li>
+        </ul>
+      </nav>
+     </header>
+        )
+    }
+}
+
+export default Navbar;
